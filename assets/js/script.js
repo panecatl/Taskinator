033fbff324339e0dd9fd92cf557b81a71e42d930
panecatl <ypanecatl24@gmail.com>
2022-05-17 12:50:52 -0500

    able to select criteria for task and used innerHTML to set up new list

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,30 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //adding things to the todo list
 var createTaskHandler = function(event) {
 
+    // ignoring browsers default for submit button
     event.preventDefault(); 
-    
 
+    // allowing us to get the words in the text
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    var taskNameInput = document.querySelector("input[name='task-name']").value; 
+
+    // allows us to choose a web, print, other property
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create dic to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
