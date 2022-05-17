67b292d9008810bf9f40c7826da9258812e93c2a
panecatl <ypanecatl24@gmail.com>
2022-05-17 12:26:55 -0500

    fixing submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDOEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //adding things to the todo list
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault(); 
+    
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
-    tasksToDOEl.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 //how the new item gets added to the list after a button click
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
