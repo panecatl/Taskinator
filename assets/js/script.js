ae621af6f5f90adc2595c1e0fe397198d227c085
panecatl <ypanecatl24@gmail.com>
2022-05-19 12:35:32 -0500

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
+// tasks in progress
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+// tasks completed
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 //adding things to the todo list
 var createFormHandler = function(event) {
@@ -172,4 +176,28 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 };
 
+var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    // get the currently selected option's value and convert to lowercase 
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element base on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler); 
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
