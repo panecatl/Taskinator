89a2742c775d9eb4a1ade77f22bcb78181817751
panecatl <ypanecatl24@gmail.com>
2022-05-19 12:14:19 -0500

    update button fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,34 @@ var taskDataObj = {
     type: taskTypeInput
 };
 
-createTaskEl(taskDataObj);
+var isEdit = formEl.hasAttribute("data-task-id");
+
+// has data attribute, so get task id and cakk function to complete edit process
+if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+}
+// no data attribute, so create object as normal and pass createTaskEl function
+else {
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);
+}
+};
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matcing task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // set new values 
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+    alert("Task Updated!");
+    // reset form by removing the task id abd changing the button text back
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function(taskDataObj) {
